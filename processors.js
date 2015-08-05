4015081cf62ca1043ffc3d6cc71c27e4c99ba8bb
Sean Hogan <shogun70@gmail.com>
2015-08-05 18:06:35 +1000

    Microdata queries are now based on elements -    the scope and property descriptors are keyed of element.__microdata__
    This means the microdata provider can return elements
       rather than descriptors

--- processors.js
+++ processors.js
@@ -621,63 +621,44 @@ function walkTree(root, skipRoot, callback) { // callback(el) must return NodeFi
 	}
 }
 
+// TODO copied from DOMSprockets. Could be a generic "class"
 
-function getView(rootNode) {
-	if (!rootNode) rootNode = document;
+var nodeIdProperty = '__microdata__';
+var nodeCount = 0; // used to generated node IDs
+var nodeStorage = {}; // hash of storage for nodes, keyed off `nodeIdProperty`
 
-	return getScopeDesc(rootNode);
-	
-
-	function getScopeDesc(scopeEl) {
-		var scopeDesc = {
-			element: scopeEl,
-			isScope: true,
-			type: scopeEl.nodeType === 1 || _.words(scopeEl.getAttribute('itemtype')),
-			properties: createHTMLPropertiesCollection(),
-			childScopes: []
-		}
-
-		walkTree(scopeEl, true, function(el) {
-			var isScope = el.hasAttribute('itemscope');
-			var propName = el.getAttribute('itemprop');
-			if (!(isScope || propName)) return NodeFilter.FILTER_SKIP;
-			
-			var item = isScope ? getScopeDesc(el) : getPropDesc(el);
-			if (propName) scopeDesc.properties.addNamedItem(propName, item);
-			else scopeDesc.childScopes.push(item);
-
-			return isScope ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_ACCEPT;
-		});
-
-		return scopeDesc;
-	}
-	
+var uniqueId = function(node) {
+	var nodeId = node[nodeIdProperty];
+	if (nodeId) return nodeId;
+	nodeId = nodeCount++; // TODO stringify??
+	node[nodeIdProperty] = new String(nodeId); // NOTE so that node cloning in old IE doesn't copy the node ID property
+	return nodeId;
 }
 
-function getPropDesc(el) {
-	var name = el.getAttribute('itemprop');
-	
-	var prop = {
-		element: el,
-		name: name,
-		value: getValue(el)
-	}
-	
-	return prop;
+var setData = function(node, data) { // FIXME assert node is element
+	var nodeId = uniqueId(node);
+	nodeStorage[nodeId] = data;
 }
 
-function getValue(el) {
-	var tagName = el.tagName.toLowerCase();
-	var attrName = valueAttr[tagName];
-	if (attrName) return el[attrName] || el.getAttribute(attrName);
-
-	return el;
+var hasData = function(node) {
+	var nodeId = node[nodeIdProperty];
+	return !nodeId ? false : nodeId in nodeStorage;
 }
 
-function getItems(scope, type) {
+var getData = function(node) { // TODO should this throw if no data?
+	var nodeId = node[nodeIdProperty];
+	if (!nodeId) return;
+	return nodeStorage[nodeId];
+}
+
+
+function getItems(rootNode, type) {
+	if (!hasData(rootNode)) parse(rootNode);
+
+	var scope = getData(rootNode);
 	var typeList = 
 		(typeof type === 'string') ? _.words(_.trim(type)) :
-		type.length ? type :
+		type && type.length ? type :
 		[];
 			
 	var resultList = [];
@@ -685,22 +666,94 @@ function getItems(scope, type) {
 	_.forEach(scope.properties.names, function(propName) {
 		var propList = scope.properties.namedItem(propName);
 		_.forEach(propList, function(prop) {
-			if (prop.isScope) [].push.apply(resultList, getItems(prop, typeList));
+			if (prop.isScope) [].push.apply(resultList, getItems(prop.element, typeList));
 		});
 	});
 
 	_.forEach(scope.childScopes, function(scope) {
 		if (!typeList.length || intersects(scope.type, typeList)) resultList.push(scope);
-		[].push.apply(resultList, getItems(scope, typeList));
+		[].push.apply(resultList, getItems(scope.element, typeList));
 	});
 
+	// now convert descriptors back to nodes
+	_.forEach(resultList, function(desc, i) {
+		resultList[i] = desc.element;
+	});
 	return resultList;
+}
+
+function getProperties(el) {
+	if (!hasData(el)) return;
+	var desc = getData(el);
+	if (!desc.isScope) return;
+	return desc.properties;
+}
+
+function parse(rootNode) {
+	if (!rootNode) rootNode = document;
+	var desc = getScopeDesc(rootNode);
+}
+
+function getScopeDesc(scopeEl) {
+	if (hasData(scopeEl)) return getData(scopeEl);
+	
+	var scopeDesc = {
+		element: scopeEl,
+		isScope: true,
+		type: scopeEl.nodeType === 1 || _.words(scopeEl.getAttribute('itemtype')),
+		properties: createHTMLPropertiesCollection(),
+		childScopes: []
+	}
+
+	walkTree(scopeEl, true, function(el) {
+		var isScope = el.hasAttribute('itemscope');
+		var propName = el.getAttribute('itemprop');
+		if (!(isScope || propName)) return NodeFilter.FILTER_SKIP;
+		
+		var item = isScope ? getScopeDesc(el) : getPropDesc(el);
+		if (propName) scopeDesc.properties.addNamedItem(propName, el);
+		else scopeDesc.childScopes.push(el);
+
+		return isScope ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_ACCEPT;
+	});
+
+	setData(scopeEl, scopeDesc);
+	return scopeDesc;
+}
+	
+function getValue(el) {
+	if (hasData(el)) return getData(el).value;
+	var desc = getPropDesc(el);
+	setData(el, desc);
+	return desc.value;
+}
+
+function getPropDesc(el) {
+	if (hasData(el)) return getData(el);
+
+	var name = el.getAttribute('itemprop');
+	
+	var prop = {
+		name: name,
+		value: evaluate(el)
+	}
+	
+	setData(el, prop);
+	return prop;
+}
+
+function evaluate(el) {
+	var tagName = el.tagName.toLowerCase();
+	var attrName = valueAttr[tagName];
+	if (attrName) return el[attrName] || el.getAttribute(attrName);
+
+	return el;
 }
 
 function createHTMLPropertiesCollection() {
 	var list = [];
 	list.names = [];
-	list.byName = {};
+	list.nodeLists = {};
 	_.assign(list, HTMLPropertiesCollection.prototype);
 	return list;
 }
@@ -709,16 +762,16 @@ var HTMLPropertiesCollection = function() {}
 _.assign(HTMLPropertiesCollection.prototype, {
 
 namedItem: function(name) {
-	return this.byName[name];
+	return this.nodeLists[name];
 },
 
-addNamedItem: function(name, item) {
-	this.push(item);
-	if (!this.byName[name]) {
-		this.byName[name] = [];
+addNamedItem: function(name, el) {
+	this.push(el);
+	if (!this.nodeLists[name]) {
+		this.nodeLists[name] = [];
 		this.names.push(name);
 	}
-	this.byName[name].push(item);
+	this.nodeLists[name].push(el);
 }
 
 });
@@ -733,8 +786,9 @@ _.forEach(_.words("meta@content link@href a@href area@href img@src video@src aud
 
 return {
 
-getView: getView,
-getItems: getItems
+getItems: getItems,
+getProperties: getProperties,
+getValue: getValue
 
 }
 
@@ -748,11 +802,12 @@ function MicrodataDecoder() {}
 _.defaults(MicrodataDecoder.prototype, {
 
 init: function(node) {
-	this.view = Microdata.getView(node);
+	Microdata.getItems(node);
+	this.rootNode = node;
 },
 
 evaluate: function(query, context, variables, wantArray) {
-	if (!context) context = this.view;
+	if (!context) context = this.rootNode;
 
 	var query = _.trim(query);
 	var startAtRoot = false;
@@ -769,33 +824,36 @@ evaluate: function(query, context, variables, wantArray) {
 	}
 	pathParts = _.words(_.trim(query));
 	
-	var scopes;
+	var nodes;
 	if (baseSchema) {
 		if (startAtRoot) context = this.view;
-		scopes = Microdata.getItems(context, baseSchema);	
+		nodes = Microdata.getItems(context, baseSchema);	
 	}
-	else scopes = [ context ];
+	else nodes = [ context ];
 
-	var resultList = scopes;
+	var resultList = nodes;
 	_.forEach(pathParts, function(relPath, i) {
 		var parents = resultList;
 		resultList = [];
-		_.forEach(parents, function(desc) {
-			var props = desc.properties.namedItem(relPath);
-			if (props) [].push.apply(resultList, props);
+		_.forEach(parents, function(el) {
+			var props = Microdata.getProperties(el);
+			if (!props) return;
+			var nodeList = props.namedItem(relPath);
+			if (!nodeList) return;
+			[].push.apply(resultList, nodeList);
 		});
 	});
-	resultList = _.map(resultList, function(desc) {
-		if (desc.isScope) return desc;
-		return desc.value;
+
+	// now convert elements to values
+	resultList = _.map(resultList, function(el) {
+		var props = Microdata.getProperties(el);
+		if (props) return el;
+		return Microdata.getValue(el);
 	});
 
 	if (wantArray) return resultList;
 
-	var item = resultList[0];
-	var value = item && (typeof item === 'object') && ('value' in item) ? item.value : item;
-
-	return value;
+	return resultList[0];
 }
 
 });
