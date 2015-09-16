6ec1fe3f047685d645a50401779cfd4abbef7895
Sean Hogan <shogun70@gmail.com>
2015-09-16 15:23:39 +1000

    Remove `controllers` from libHyperFrameset to DOMSprockets.

--- libHyperFrameset.js
+++ libHyperFrameset.js
@@ -1165,6 +1165,7 @@ return scheduler;
 /* BEGIN HFrameset code */
 
 var sprockets = Meeko.sprockets;
+var controllers = Meeko.controllers;
 
 var framer = Meeko.framer = (function() {
 
@@ -1603,51 +1604,6 @@ function rebaseURL(url, baseURL) {
 return HFramesetDefinition;	
 })();
  
-var controllers = (function() {
-
-return {
-
-values: {},
-
-listeners: {},
-
-create: function(name) {
-	this.values[name] = [];
-	this.listeners[name] = [];
-},
-
-has: function(name) {
-	return (name in this.values);
-},
-
-get: function(name) { 
-	if (!this.has(name)) throw name + ' is not a registered controller';
-	return this.values[name];
-},
-
-set: function(name, value) {
-	if (!this.has(name)) throw name + ' is not a registered controller';
-	if (value === false || value == null) value = [];
-	else if (typeof value === 'string' || !('length' in value)) value = [ value ];
-	var oldValue = this.values[name];
-	if (_.difference(value, oldValue).length <= 0) return;
-	this.values[name] = value;
-	_.forEach(this.listeners[name], function(listener) {
-		Task.asap(function() { listener(value); });
-	});	
-},
-
-listen: function(name, listener) {
-	if (!this.has(name)) throw name + ' is not a registered controller';
-	this.listeners[name].push(listener);
-	var value = this.values[name];
-	Task.asap(function() { listener(value) });
-}
-
-};
-
-})();
-
 var Layer = (function() {
 
 var Layer = sprockets.evolve(sprockets.RoleType, {
