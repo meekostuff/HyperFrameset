c71a9d6f1db0151c06bad5e63c73830f91cb3cb8
Sean Hogan <shogun70@gmail.com>
2019-10-19 13:46:22 +1100

    Assume DOMParser#parseFromString() supports 'text/html'.

--- HyperFrameset.js
+++ HyperFrameset.js
@@ -2882,26 +2882,10 @@
 	};
 
 	/*
-		HTML_IN_DOMPARSER indicates if DOMParser supports 'text/html' parsing. Historically only Firefox did.
-		Cross-browser support coming? https://developer.mozilla.org/en-US/docs/Web/API/DOMParser#Browser_compatibility
-	*/
-	const HTML_IN_DOMPARSER = (function() {
-
-		try {
-			let doc = (new DOMParser).parseFromString('', 'text/html');
-			return !!doc;
-		}
-		catch(err) { return false; }
-
-	})();
-
-
-	/*
 		normalize() is called between html-parsing (internal) and document normalising (external function).
 		It is called after using the native parser:
 		- with DOMParser#parseFromString(), see htmlParser#nativeParser()
 		- with XMLHttpRequest & xhr.responseType='document', see httpProxy's request()
-		The innerHTMLParser also uses this call
 	*/
 	function normalize(doc, details) { 
 
@@ -2974,31 +2958,8 @@
 
 	}
 
-	function innerHTMLParser(html, details) {
-		return Promise.pipe(null, [
-			
-		function() {
-			let doc = createHTMLDocument('');
-			let docElement = doc.documentElement;
-			docElement.innerHTML = html;
-			let m = html.match(/<html(?=\s|>)(?:[^>]*)>/i); // WARN this assumes there are no comments containing '<html' and no attributes containing '>'.
-			let div = document.createElement('div');
-			div.innerHTML = m[0].replace(/^<html/i, '<div');
-			let htmlElement = div.firstChild;
-			copyAttributes(docElement, htmlElement);
-			return doc;
-		},
-		
-		function(doc) {
-			return normalize(doc, details);
-		}
-		
-		]);
-	}
-
 	var htmlParser = {
-		HTML_IN_DOMPARSER,
-		parse: HTML_IN_DOMPARSER ? nativeParser : innerHTMLParser,
+		parse: nativeParser,
 		normalize
 	};
 
--- src/Meeko/htmlParser.mjs
+++ src/Meeko/htmlParser.mjs
@@ -5,26 +5,10 @@ import * as DOM from './DOM.mjs';
 import Promise from './Promise.mjs';
 
 /*
-	HTML_IN_DOMPARSER indicates if DOMParser supports 'text/html' parsing. Historically only Firefox did.
-	Cross-browser support coming? https://developer.mozilla.org/en-US/docs/Web/API/DOMParser#Browser_compatibility
-*/
-const HTML_IN_DOMPARSER = (function() {
-
-	try {
-		let doc = (new DOMParser).parseFromString('', 'text/html');
-		return !!doc;
-	}
-	catch(err) { return false; }
-
-})();
-
-
-/*
 	normalize() is called between html-parsing (internal) and document normalising (external function).
 	It is called after using the native parser:
 	- with DOMParser#parseFromString(), see htmlParser#nativeParser()
 	- with XMLHttpRequest & xhr.responseType='document', see httpProxy's request()
-	The innerHTMLParser also uses this call
 */
 function normalize(doc, details) { 
 
@@ -97,30 +81,7 @@ function nativeParser(html, details) {
 
 }
 
-function innerHTMLParser(html, details) {
-	return Promise.pipe(null, [
-		
-	function() {
-		let doc = DOM.createHTMLDocument('');
-		let docElement = doc.documentElement;
-		docElement.innerHTML = html;
-		let m = html.match(/<html(?=\s|>)(?:[^>]*)>/i); // WARN this assumes there are no comments containing '<html' and no attributes containing '>'.
-		let div = document.createElement('div');
-		div.innerHTML = m[0].replace(/^<html/i, '<div');
-		let htmlElement = div.firstChild;
-		DOM.copyAttributes(docElement, htmlElement);
-		return doc;
-	},
-	
-	function(doc) {
-		return normalize(doc, details);
-	}
-	
-	]);
-}
-
 export default {
-	HTML_IN_DOMPARSER,
-	parse: HTML_IN_DOMPARSER ? nativeParser : innerHTMLParser,
+	parse: nativeParser,
 	normalize
 }
